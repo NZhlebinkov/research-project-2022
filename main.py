0225f1d380ae3b819bf3ecff472fe639dae94e76
Nikko <nzhlebinkov@gmail.com>
2022-06-03 15:51:41 +0200

    Fea: Retrieve and augment all files from a folder

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 import librosa
 import soundfile
 from vtlp import VtlpAug
+from os import listdir
+from os.path import isfile, join
 
-fileNames = open("../wavSource")
+sourceDir = "../../corpus-data/wavs/q/"
+outputDir = "../augmented/"
+onlyFiles = [f for f in listdir(sourceDir) if isfile(join(sourceDir, f))]
 
-for fileName in fileNames:
-    fileName = str.strip(fileName)
-    filePath = "../" + fileName
+print("Filenames retrieved")
+
+for fileName in onlyFiles:
+    filePath = join(sourceDir, fileName)
     data, samplerate = librosa.load(filePath)
 
     aug = VtlpAug(samplerate, factor_range=(0.9, 1.1), zone=(0, 1), coverage=1)
     augmented, warp_factor = aug.augment(data)
 
-    outputPath = "../augmented/" + fileName
+    outputPath = join(outputDir, fileName)
     soundfile.write(outputPath, augmented, samplerate)
     print("This is warp: " + str(warp_factor))
+
+# fileNames = open("../warps")
